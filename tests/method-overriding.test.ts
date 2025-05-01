d6fc5830fb36ac47e5e34afcab8d7ac00633abc4
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-02 06:54:36 +0700

    Super Method

--- tests/method-overriding.test.ts
+++ tests/method-overriding.test.ts
@@ -14,7 +14,9 @@ describe('Method Overriding', () => {
     class Manager extends Employee {
 
         sayHello(name: string): void {
-            console.info(`Hello ${name}, my name is ${this.name}, i am your manager`);
+            // console.info(`Hello ${name}, my name is ${this.name}, i am your manager`);
+            super.sayHello(name);
+            console.info(`And, I am your manager`);
         }
     }
 
