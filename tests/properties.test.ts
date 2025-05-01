fb851df4d84e7d30d8081f2d890aaf800d609cec
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-02 01:03:00 +0700

    Methods

--- tests/properties.test.ts
+++ tests/properties.test.ts
@@ -8,6 +8,10 @@ describe('Properties', () => {
             this.id = id;
             this.name = name;
         }
+        
+        sayHello(name: string): void {
+            console.info(`Hello ${name}, my name is  ${this.name}`);
+        }
     }
 
     it('should can have properties', () => {
@@ -18,5 +22,10 @@ describe('Properties', () => {
         console.info(customer.name);
         console.info(customer.age);
         console.info(customer);
+    });
+
+    it('should can have methods', () => {
+        const customer = new Customer(1, "Aldi");
+        customer.sayHello("Zanuar");
     });
 });
