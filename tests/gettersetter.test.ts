6a2ca23e5b8c065b3595a063468b3feb61d71885
zanuaraldi <zanuar0101aldisyahputra@gmail.com>
2025-05-02 01:11:29 +0700

    Getter dan Setter

---
+++ tests/gettersetter.test.ts
@@ -0,0 +1,31 @@
+describe('Getter and Setter', () => {
+    class Category {
+        _name?: string;
+
+        get name(): string {
+            if(this._name){
+                return this._name;
+            } else {
+                return "empty";
+            }
+        }
+        
+        set name(value: string) {
+            if(value !== ""){
+                this._name = value;
+            }
+        }
+    }
+
+    it('should support in class', () => {
+        const category = new Category();
+        console.info(category.name);
+
+        category.name = "Aldi";
+        console.info(category.name);
+
+        category.name = "";
+        console.info(category.name);
+
+    });
+});
